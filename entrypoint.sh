289cff84a424940ed9bb79a96f14f255dcb5024c
Christopher Sims <cbarsim@gmail.com>
2025-03-10 17:22:42 -0700

    Moved uvicorn args from entryptoint to the programmatic run() call in python

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ LOG_FILE="/var/log/interp/interp_$(date +'%Y-%m-%d_%H-%M-%S').log"
 echo "entrypoint.sh - $(date +'%Y-%m-%d_%H-%M-%S')" | tee -a $LOG_FILE
 
 # Run the app and tee the output
-uvicorn src.main:app --host 0.0.0.0 --port 8080 --reload 2>&1 | tee -a $LOG_FILE
+uvicorn src.main:app 2>&1 | tee -a $LOG_FILE
