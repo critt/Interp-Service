f1ac7ab5cc53ab91ba1f92b2f3b562a3a70e3a98
Christopher Sims <cbarsim@gmail.com>
2025-03-11 15:22:01 -0700

    Fixed entrypoint and main app run invocation. Previously, I wasnt' even using the settings from app.run() in main.py. This is because the entrypoint command was still calling 'uvicorn'. The app.run() call and the entrypoing command are now correct in each other's context.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ LOG_FILE="/var/log/interp/interp_$(date +'%Y-%m-%d_%H-%M-%S').log"
 echo "entrypoint.sh - $(date +'%Y-%m-%d_%H-%M-%S')" | tee -a $LOG_FILE
 
 # Run the app and tee the output
-uvicorn src.main:app 2>&1 | tee -a $LOG_FILE
+python3 src/main.py 2>&1 | tee -a $LOG_FILE
--- src/main.py
+++ src/main.py
@@ -4,11 +4,14 @@ import socketio
 from config import settings
 from routes import router as api_router
 from socket_handlers import register_socket_handlers
+from gcp_service import GCPService
 from engineio.payload import Payload
+
 Payload.max_decode_packets = 50
 
 sio: Any = socketio.AsyncServer(async_mode="asgi")
-register_socket_handlers(sio)
+gcp_service = GCPService()
+register_socket_handlers(sio, gcp_service)
 socket_app = socketio.ASGIApp(sio)
 
 app = FastAPI()
@@ -18,4 +21,12 @@ app.mount("/", socket_app)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=settings.backend_port, timeout_keep_alive=0, http="httptools", reload=True)
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=settings.backend_port,
+        timeout_keep_alive=0,
+        http="httptools",
+        reload=True,
+    )
